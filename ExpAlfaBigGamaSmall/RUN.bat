35037f0ea3f278daf0ae86fac8580b2660ff1f22
Ben Earle <ben.earle@live.ca>
2022-11-02 14:10:16 -0400

    Add files via upload

---
+++ ExpAlfaBigGamaSmall/RUN.bat
@@ -0,0 +1,3 @@
+C:/Eclipse/plugins/CD++Builder_1.1.0/internal/simuOrig.exe  -mPopulations.ma -oPopulations.out -lPopulations.log -t00:00:20:000
+C:/Eclipse/plugins/CD++Builder_1.1.0/internal/drawlog.exe  -m'Populations.ma' -c'Populations' -l'Populations.log'  1>Populations.drw
+C:/Eclipse/plugins/CD++Builder_1.1.0/internal/GrafLog -2 -s0 -e0 -fPopulations.drw -ccolores.col
